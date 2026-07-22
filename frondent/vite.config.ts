c427aa3dcf9c740374745c97b82de613c02d7aa1
saiyappa <saiyappalokhande@gmail.com>
2026-07-22 09:30:50 +0530

    Update backend URL

--- frondent/vite.config.ts
+++ frondent/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "https://student-management-system-0vjl.onrender.com",
         changeOrigin: true,
       },
     },
