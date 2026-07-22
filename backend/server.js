38370e59906284f2537376419ecab29bab11b16e
saiyappa <saiyappalokhande@gmail.com>
2026-07-22 16:57:16 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://student-management-system-1111.netlify.app",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 app.use(express.json());
 
 // MongoDB Connection
